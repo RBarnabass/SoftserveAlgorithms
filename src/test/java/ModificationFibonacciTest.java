fb56d959b19c8b544f32068bffbcfc2fd27b1e45
Senchakmen <senchakevuch@gmail.com>
2019-01-30 16:55:57 +0200

    implements ModificationFibonacci file

---
+++ src/test/java/ModificationFibonacciTest.java
@@ -0,0 +1,28 @@
+import Algorithms.ModificationFibonacci;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModificationFibonacciTest {
+
+    @org.junit.jupiter.api.Test
+    void modificationFibonacciTest() {
+
+        int testInputA = 3;
+        int testOutputA = 1;
+
+        int testInputB = 6;
+        int testOutputB = 4;
+
+        int testInputC = 7;
+        int testOutputC = 6;
+
+        assertEquals(testOutputA, ModificationFibonacci.modificationFibonacci(testInputA));
+        assertEquals(testOutputB, ModificationFibonacci.modificationFibonacci(testInputB));
+        assertEquals(testOutputC, ModificationFibonacci.modificationFibonacci(testInputC));
+
+        assertEquals(testOutputA, ModificationFibonacci.mmodificationFibonacci(testInputA));
+        assertEquals(testOutputB, ModificationFibonacci.modificationFibonacci(testInputB));
+        assertEquals(testOutputC, ModificationFibonacci.modificationFibonacci(testInputC));
+    }
+
+}
