95cd037b54748c29323d41f6ba392129324e149b
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-01-31 00:32:15 +0200

    Realized the algorithm for painting the fence

--- src/main/java/Algorithms/PaintingTheFence.java
+++ src/main/java/Algorithms/PaintingTheFence.java
@@ -1,7 +1,20 @@
 package Algorithms;
 
+/**
+ * Class that realized logic of the ways for painting the fence
+ *
+ * @version 1.0
+ * @author Valentyn Yarmoshyk
+ */
 public class PaintingTheFence {
 
+    /**
+     * Return the number of the ways for painting the fence
+     *
+     * @param n The number of posts you need to paint
+     * @param k The number of colors you have
+     * @return The number of the ways for painting the fence
+     */
     public static long paintingTheFence(int n, int k){
 
         long[] variant = new long[n];
