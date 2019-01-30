ed3854e73e3192b9fb41148da94379e0b78c2007
octap16 <octap17@gmail.com>
2019-01-30 22:27:11 +0200

    implemented test for Insertion Test

--- src/test/java/InsertionSortTest.java
+++ src/test/java/InsertionSortTest.java
@@ -1,15 +1,15 @@
 import Algorithms.InsertionSort;
 
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class InsertionSortTest {
 
     @org.junit.jupiter.api.Test
-    void doInsertionSortTest() {
+    void doInsertionSortTest(){
 
         int[] inputArray = {52, 10, 6, 40};
         int[] expectedArray = {6, 10, 40, 52};
-
 
         assertArrayEquals(expectedArray, InsertionSort.doInsertionSort(inputArray));
     }
