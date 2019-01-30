136549b352abfae99bab9769a27caece457df5e3
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-01-30 22:28:59 +0200

    provided javadoc for countWaysToPair method

--- src/main/java/Algorithms/FriendPairs.java
+++ src/main/java/Algorithms/FriendPairs.java
@@ -1,4 +1,28 @@
 package Algorithms;
 
 public class FriendPairs {
+
+    /**
+     * Returns count of possible ways to pair/stay alone for given amount of friends.
+     *
+     * @param numberOfFriends amount of friends to cover
+     * @return count of possible ways to pair
+     * @author Oleh Volchkov
+     */
+    public static int countWaysToPair(int numberOfFriends) {
+
+        int cachedWays[] = new int[numberOfFriends + 1];
+
+        for (int i = 0; i <= numberOfFriends; i++) {
+
+            if (i <= 2) {
+                cachedWays[i] = i;
+            } else {
+                cachedWays[i] = cachedWays[i - 1] + (i - 1) * cachedWays[i - 2];
+            }
+        }
+
+        return cachedWays[numberOfFriends];
+    }
 }
+
