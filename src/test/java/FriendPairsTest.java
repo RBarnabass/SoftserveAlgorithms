7e76c6e1304577ca827b941e610b2103eef1e392
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-01-30 22:10:06 +0200

    set up test for countWaysToPair method

--- src/test/java/FriendPairsTest.java
+++ src/test/java/FriendPairsTest.java
@@ -1,2 +1,15 @@
+import Algorithms.FriendPairs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FriendPairsTest {
+
+    @org.junit.jupiter.api.Test
+    void countWaysToPairTest() {
+
+        int testInput = 3;
+        int expectedOutput = 4;
+
+        assertEquals(expectedOutput, FriendPairs.countWaysToPair(testInput));
+    }
 }
