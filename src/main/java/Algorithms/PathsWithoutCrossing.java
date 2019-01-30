c8ae7bdeae8423da358decbb032318cbdf368a16
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-01-30 21:44:13 +0200

    added method for finding n-th number from catalan sequence

--- src/main/java/Algorithms/PathsWithoutCrossing.java
+++ src/main/java/Algorithms/PathsWithoutCrossing.java
@@ -3,5 +3,33 @@ package Algorithms;
 
 public class PathsWithoutCrossing {
 
- 
+    /**
+     * @param numberOfPoints
+     * @return
+     */
+    public static int countPaths(int numberOfPoints) {
+
+        return 0;
+    }
+
+    /**
+     * @param n
+     * @return
+     */
+    public static int catalanNumber(int n) {
+
+        int[] catalan = new int[n + 1];
+
+        catalan[0] = 1;
+        catalan[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            catalan[i] = 0;
+            for (int j = 0; j < i; j++) {
+                catalan[i] += catalan[j] * catalan[i - j - 1];
+            }
+        }
+
+        return catalan[n];
+    }
 }
