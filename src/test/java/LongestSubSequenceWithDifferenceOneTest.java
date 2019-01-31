79cc3201241d8bdfe0881c389285369c551079f6
Barnabass <rastafarimood@gmail.com>
2019-01-31 13:55:37 +0200

    created test cases for longestSubSeq algorithm refactor

--- src/test/java/LongestSubSequenceWithDifferenceOneTest.java
+++ src/test/java/LongestSubSequenceWithDifferenceOneTest.java
@@ -23,35 +23,35 @@ public class LongestSubSequenceWithDifferenceOneTest {
 
     @Test public void oneElementTest() {
         test = new int[] {0};
-        assertEquals(1, sequence.get(test));
+        assertEquals(1, sequence.getTheLongestOne(test));
     }
     @Test public void twoElementTest() {
         test = new int[] {0, 2};
-        assertEquals(1, sequence.get(test));
+        assertEquals(1, sequence.getTheLongestOne(test));
     }
 
     @Test public void twoSequenceElementTest() {
         test = new int[] {0, 1};
-        assertEquals(2, sequence.get(test));
+        assertEquals(2, sequence.getTheLongestOne(test));
     }
     @Test public void fiveSequenceElementTest() {
         test = new int[] {0, 1, 2, 3, 4};
-        assertEquals(5, sequence.get(test));
+        assertEquals(5, sequence.getTheLongestOne(test));
     }
     @Test public void fiveSequenceMixedElementTest() {
         test = new int[] {0, 3, 1, 4, 2, 5, 3, 6, 4};
-        assertEquals(5, sequence.get(test));
+        assertEquals(5, sequence.getTheLongestOne(test));
     }
     @Test public void fiveSequenceDoubleElementTest() {
         test = new int[] {0, 7, 1, 8, 2, 9, 3, 10, 4, 11};
-        assertEquals(5, sequence.get(test));
+        assertEquals(5, sequence.getTheLongestOne(test));
     }
     @Test public void sixSequenceLongerThenFiveElementTest() {
         test = new int[] {0, 7, 1, 8, 2, 9, 3, 10, 4, 11, 6, 12};
-        assertEquals(6, sequence.get(test));
+        assertEquals(6, sequence.getTheLongestOne(test));
     }
     @Test public void tenSequenceWaveElementTest() {
         test = new int[] {0, 7, 1, 8, 2, 9, 1, 10, 2, 11, 3, -1, 4, 0, 3, 1, 4, 2, 5};
-        assertEquals(10, sequence.get(test));
+        assertEquals(10, sequence.getTheLongestOne(test));
     }
 }
