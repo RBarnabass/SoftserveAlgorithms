41a99f1afed1bf897e07732c15831c19b6a8f6d7
Senchakmen <senchakevuch@gmail.com>
2019-01-31 14:54:45 +0200

    Implements OptimizedPaintingFenceTest

--- src/test/java/OptimizedPaintingFenceTest.java
+++ src/test/java/OptimizedPaintingFenceTest.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import Algorithms.OptimizedPaintingFence;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OptimizedPaintingFenceTest {
+
+    @org.junit.jupiter.api.Test
+    void optimizedPaintingFenceTest() {
+
+        int testInputN = 2;
+        int testInputK = 4;
+        long testOutput = 16;
+
+        assertEquals(testOutput, OptimizedPaintingFence.optimizedPaintingFence(testInputN, testInputK));
+    }
+
 }
