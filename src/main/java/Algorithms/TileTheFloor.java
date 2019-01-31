41a3f851afd2358b6fdc2de4d67dfca03af8ab31
Arsen <arsenemarkiv@gmail.com>
2019-01-31 10:39:10 +0200

    implemented ways to tile the floor

---
+++ src/main/java/Algorithms/TileTheFloor.java
@@ -0,0 +1,29 @@
+package Algorithms;
+
+public class TileTheFloor {
+
+    /**
+     * Method for count ways to tile the floor
+     *
+     * @param n first measure parameter of floor:
+     * @param m second measure parameter of floor and tile
+     * @return return count of ways to tile the floor
+     */
+    public static int countWays(int n, int m) {
+
+        int count[] = new int[n + 1];
+        count[0] = 0;
+
+        int i;
+        for (i = 1; i <= n; i++) {
+            if (i > m)
+                count[i] = count[i - 1] + count[i - m];
+            else if (i < m) {
+                count[i] = 1;
+            } else {
+                count[i] = 2;
+            }
+        }
+        return count[n];
+    }
+}
---
+++ src/test/java/TileTheFloorTest.java
@@ -0,0 +1,28 @@
+import Algorithms.TileTheFloor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class TileTheFloorTest {
+
+    @org.junit.jupiter.api.Test
+    void tileTheFloor() {
+
+        int n1 = 4;
+        int m1 = 6;
+        int numOfWays1 = 1;
+
+        int n2 = 3;
+        int m2 = 2;
+        int numOfWays2 = 3;
+
+        int n3 = 10;
+        int m3 = 4;
+        int numOfWays3 = 14;
+
+        assertEquals(numOfWays1, TileTheFloor.countWays(n1, m1));
+        assertEquals(numOfWays2, TileTheFloor.countWays(n2, m2));
+        assertEquals(numOfWays3, TileTheFloor.countWays(n3, m3));
+    }
+
+}
