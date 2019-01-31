2582d9e7d161122093dc75a7e98066a7ca06f5c4
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-01-31 17:07:01 +0200

    Merge sort have done completely v1.0

--- src/main/java/algorithms/MergeSort.java
+++ src/main/java/algorithms/MergeSort.java
@@ -5,6 +5,8 @@ import strategy.AlgorithmStrategy;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static utilities.util.writeArray;
+
 /**
  * Class thar sorts the array by an algorithm Merge Sort
  *
@@ -12,57 +14,6 @@ import java.util.Scanner;
  * @author Valentyn Yarmoshyk
  */
 public class MergeSort implements AlgorithmStrategy {
-
-    /**
-     * Return the length of the array
-     *
-     * @param scanner For input data
-     * @return The length of the array
-     * @author Valentyn Yarmoshyk
-     */
-    public static int lengthOfArray(Scanner scanner) {
-        System.out.print("How much elements: ");
-
-        while (!scanner.hasNextInt()) {
-            System.out.println("Int, please!");
-            scanner.nextLine();
-        }
-        int length = scanner.nextInt();
-        System.out.println("The length of yout array is " + length);
-
-        return length;
-    }
-
-    /**
-     * Return the array that you entered
-     *
-     * @param scanner For input data
-     * @return An array
-     * @author Valentyn Yarmoshyk
-     */
-    public static int[] writeArray(Scanner scanner) {
-
-        int length = lengthOfArray(scanner);
-
-        int[] array = new int[length];
-        int i = 0;
-
-        while (i < array.length) {
-
-            System.out.print("Enter the " + i + " element: ");
-
-            while (!scanner.hasNextInt()) {
-                System.out.println("Int, please!");
-                scanner.nextLine();
-            }
-
-            array[i] = scanner.nextInt();
-            i++;
-
-        }
-        System.out.println("Your array: " + Arrays.toString(array));
-        return array;
-    }
 
     /**
      * Realise the merge sort
