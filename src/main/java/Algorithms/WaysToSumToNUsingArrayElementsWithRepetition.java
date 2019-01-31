5867afeb227cfc2374103d1a85fb0fdd47edbbe5
octap16 <octap17@gmail.com>
2019-01-31 14:24:37 +0200

    Change the annotation to the methods

--- src/main/java/Algorithms/WaysToSumToNUsingArrayElementsWithRepetition.java
+++ src/main/java/Algorithms/WaysToSumToNUsingArrayElementsWithRepetition.java
@@ -1,15 +1,20 @@
 package Algorithms;
 
+/**
+ * @author Ostap Vdovychyn
+ * @version 1.0
+ */
 public class WaysToSumToNUsingArrayElementsWithRepetition {
     /**
      * Return number which show how many ways we have to represent one number with a given array of numbers
      *
-     * @param arr and number Input array and the number which has to be represented
+     * @param arr    Input array, elements in the array will be added
+     * @param number Input number which has to be represented
      * @return count[number] of possible ways we have to represent one number with a given array of numbers
      * @author Ostap Vdovychyn
      */
     public static int waysToCount(int arr[], int number) {
-        int count[] = new int[number+1];
+        int count[] = new int[number + 1];
         count[0] = 1;
         for (int i = 1; i <= number; i++) {
             for (int j = 0; j < arr.length; j++) {
