f42c719a10e59ccde3acec66b29797895aff6e44
Ira Shturyn <ira.shturyn@gmail.com>
2019-01-31 02:07:40 +0200

    implement Ways to write n as sum of two or more positive integers

---
+++ src/main/java/Algorithms/CountWaysOfSum.java
@@ -0,0 +1,20 @@
+package Algorithms;
+
+public class CountWaysOfSum {
+    /**
+     * Returns how many ways is possible to calculate n with positive integers.
+     *
+     * @param n positive integer
+     * @return count of ways
+     */
+    public int countWays(int n) {
+        int[] ways = new int[n + 1];
+        ways[0] = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j <= n; j++) {
+                ways[j] += ways[j - i];
+            }
+        }
+        return ways[n];
+    }
+}
