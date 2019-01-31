9c54c578c061bd960c7fd49bf5f7cf4981dccdc5
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-01-31 15:54:34 +0200

    implemented execute() method from strategy interface

--- src/main/java/algorithms/WaysToCoverDistance.java
+++ src/main/java/algorithms/WaysToCoverDistance.java
@@ -1,20 +1,43 @@
-package Algorithms;
+package algorithms;
 
-public class WaysToCoverDistance {
+import strategy.AlgorithmStrategy;
+
+import java.util.Scanner;
+
+public class WaysToCoverDistance implements AlgorithmStrategy {
+
+    public void execute() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter distance to cover: ");
+
+        int distance = sc.nextInt();
+        int ways = calculateWays(distance);
+
+        StringBuilder result = new StringBuilder()
+                .append("There is ").append(ways)
+                .append(" number of ways for covering given distance.");
+
+        System.out.println(result);
+    }
 
     /**
      * Returns number of ways for covering given distance.
      *
-     * @param   distance    distance to cover
-     * @return              number of possible ways to cover the distance
-     * @author              Oleh Volchkov
+     * @param distance distance to cover
+     * @return number of possible ways to cover the distance
+     * @author Oleh Volchkov
      */
     public static int calculateWays(int distance) {
 
-        if (distance < 0)
+        if (distance < 0) {
             return 0;
-        if (distance == 0)
+        }
+
+        if (distance == 0) {
             return 1;
+        }
 
         return calculateWays(distance - 1) +
                 calculateWays(distance - 2) +
