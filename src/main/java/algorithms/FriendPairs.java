ccbe9fd7243944cf49f2904b91c8c4e5ebc62edd
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-02-01 14:18:16 +0200

    implemented input validation

--- src/main/java/algorithms/FriendPairs.java
+++ src/main/java/algorithms/FriendPairs.java
@@ -2,6 +2,7 @@ package algorithms;
 
 import strategy.IAlgorithmStrategy;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -21,14 +22,40 @@ public class FriendPairs implements IAlgorithmStrategy {
 
         System.out.println("Enter amount of friends: ");
 
-        int friends = sc.nextInt();
-        int ways = countWaysToPair(friends);
+        int friendsCount = getUserInput();
+        int ways = countWaysToPair(friendsCount);
 
         StringBuilder result = new StringBuilder()
                 .append("There is ").append(ways)
                 .append(" number of ways to pair for given amount of friends.");
 
         System.out.println(result);
+    }
+
+    /**
+     * Get correct input from user.
+     *
+     * @return friends from user input
+     */
+    private int getUserInput() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int friendsCount = -1;
+
+        try {
+            friendsCount = sc.nextInt();
+
+            if(friendsCount < 0) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+
+            System.out.println("Incorrect input. Please, provide positive integer value:");
+            friendsCount = getUserInput();
+        }
+
+        return friendsCount;
     }
 
     /**
