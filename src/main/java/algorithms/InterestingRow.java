936f4e14a887a7b00bc3ad5fda9c4dcf6c2bb5af
Nazar <nazarmavko@gmail.com>
2019-02-01 14:31:09 +0200

    implmented HighLowEfforts.java

--- src/main/java/algorithms/InterestingRow.java
+++ src/main/java/algorithms/InterestingRow.java
@@ -1,13 +1,22 @@
 package algorithms;
 
-public class InterestingRow {
-    
+import strategy.IAlgorithmStrategy;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Class that returns member of position in Interesting Row sequence
+ *
+ * @author Nazar Mavko
+ */
+public class InterestingRow implements IAlgorithmStrategy {
+
     /**
      * Returns the member of position in Fibonacci sequence
      *
      * @param number position in interesting row
      * @return member of position in interesting row sequence
-     * @author Nazar Mavko
      */
     public static int findIntrestingRowMember(int number) {
         int[] interestingRowArray = new int[number + 2];
@@ -17,5 +26,23 @@ public class InterestingRow {
             interestingRowArray[i] = 2 * interestingRowArray[i - 2] + interestingRowArray[i - 1];
         }
         return interestingRowArray[number];
+    }
+
+    public void execute() {
+        while (true) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Please enter a position for a number in Fibonacci sequence you want to know");
+                int number = sc.nextInt();
+                if (number > 0) {
+                    System.out.println(findIntrestingRowMember(number));
+                } else {
+                    System.out.println("Please enter number that >0");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println(e);
+                System.out.println("Wrong type of input.Please use int type!");
+            }
+        }
     }
 }
