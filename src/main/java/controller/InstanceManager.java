954b26d078a82df3b0359242cdbde9e3090ca9b0
Barnabass <rastafarimood@gmail.com>
2019-02-03 10:50:22 +0200

    new src/main/java/controller/InstanceManager.java

---
+++ src/main/java/controller/InstanceManager.java
@@ -0,0 +1,17 @@
+package controller;
+
+import strategy.IAlgorithmStrategy;
+
+/**
+ * This is the interface for instance managing strategy.
+ */
+public interface InstanceManager {
+
+    /**
+     * This method should return algorithm instance by its name.
+     *
+     * @param name algorithm name.
+     * @return     algorithm instance.
+     */
+    IAlgorithmStrategy getAlgorithm(Algorithm name);
+}
