e882770a9acbeedb9b2f5f8f553a6dc12b2ec8dd
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-04 16:45:38 +0200

    Delete 'static' from test methods

--- src/test/java/algorithms/CoutingSortTest.java
+++ src/test/java/algorithms/CoutingSortTest.java
@@ -7,9 +7,11 @@ public class CoutingSortTest {
     @org.junit.jupiter.api.Test
     public void countingSortTest() {
 
+        CoutingSort coutingSort = new CoutingSort();
+
         int[] array = {5, 10,1005, 9, -10, 20, 45, -789};
         int[] expectedArray = {-789, -10, 5, 9, 10, 20, 45, 1005};
-        assertArrayEquals(expectedArray, CoutingSort.countingSort(array));
+        assertArrayEquals(expectedArray, coutingSort.countingSort(array));
 
     }
 }
