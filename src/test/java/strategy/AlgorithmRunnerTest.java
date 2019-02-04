cf237664e6b5a2b1b977c8375433516856f1351b
Barnabass <rastafarimood@gmail.com>
2019-02-04 09:10:39 +0200

    src/test/java/strategy/AlgorithmRunnerTest.java refactor

--- src/test/java/strategy/AlgorithmRunnerTest.java
+++ src/test/java/strategy/AlgorithmRunnerTest.java
@@ -1,14 +1,13 @@
 package strategy;
 
 import org.junit.jupiter.api.Test;
-
 import static org.mockito.Mockito.*;
 
 public class AlgorithmRunnerTest {
 
     private AlgorithmRunner runner = mock(AlgorithmRunner.class);
 
-    @Test public void test() {
+    @Test public void callTest() {
         doNothing().when(runner).run();
         runner.run();
         verify(runner, times(1)).run();
