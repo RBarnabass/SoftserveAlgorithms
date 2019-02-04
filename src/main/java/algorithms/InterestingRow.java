f57219047db8b33cc93f681eaf278d1a47e7daaa
Nazar <nazarmavko@gmail.com>
2019-02-04 21:38:51 +0200

    implmentation of InterestinRow.class

--- src/main/java/algorithms/InterestingRow.java
+++ src/main/java/algorithms/InterestingRow.java
@@ -17,7 +17,7 @@ public class InterestingRow implements IAlgorithmStrategy {
      * @param number position in interesting row
      * @return member of position in interesting row sequence
      */
-    public static int findIntrestingRowMember(int number) {
+    public int findIntrestingRowMember(int number) {
         int[] interestingRowArray = new int[number + 2];
         interestingRowArray[1] = 1;
         interestingRowArray[2] = 1;
@@ -30,6 +30,7 @@ public class InterestingRow implements IAlgorithmStrategy {
     /**
      * Runner method for execution of algorithm.
      */
+    @Override
     public void execute() {
         System.out.println("Please enter a position for a number in Interesting Row sequence you want to know");
         int minNumber = 0;
