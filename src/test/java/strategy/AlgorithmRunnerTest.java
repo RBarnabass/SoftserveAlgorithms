dc80c8d7334667d84386a30f36ede29d5e6c6306
Barnabass <rastafarimood@gmail.com>
2019-02-04 09:06:22 +0200

    src/test/java/strategy/AlgorithmRunnerTest.java new test

---
+++ src/test/java/strategy/AlgorithmRunnerTest.java
@@ -0,0 +1,16 @@
+package strategy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class AlgorithmRunnerTest {
+
+    private AlgorithmRunner runner = mock(AlgorithmRunner.class);
+
+    @Test public void test() {
+        doNothing().when(runner).run();
+        runner.run();
+        verify(runner, times(1)).run();
+    }
+}
