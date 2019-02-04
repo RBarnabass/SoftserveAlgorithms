12d68dc953077349c293038b128c9fcdd7a6df90
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-04 18:29:44 +0200

    tilethefloor

--- src/main/java/algorithms/TileTheFloor.java
+++ src/main/java/algorithms/TileTheFloor.java
@@ -22,8 +22,7 @@ public class TileTheFloor implements IAlgorithmStrategy {
         int[] count = new int[n + 1];
         count[0] = 0;
 
-        int i;
-        for (i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i > m) {
                 count[i] = count[i - 1] + count[i - m];
             } else if (i < m) {
