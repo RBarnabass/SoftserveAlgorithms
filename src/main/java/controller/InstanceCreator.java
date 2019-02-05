655c21af47928716e8fa349c478dab5d74e66f4e
Barnabass <rastafarimood@gmail.com>
2019-02-05 06:26:15 +0200

    src/main/java/controller/InstanceCreator.java ref

--- src/main/java/controller/InstanceCreator.java
+++ src/main/java/controller/InstanceCreator.java
@@ -33,7 +33,7 @@ public class InstanceCreator implements InstanceManager {
     /**
      * Map that contain all algorithms.
      */
-    private static final Map<Algorithm, IAlgorithmStrategy> algorithms = new HashMap<>();
+    private static final Map<AlgorithmName, IAlgorithmStrategy> algorithms = new HashMap<>();
 
     /**
      * Constructor which starts init method.
@@ -48,7 +48,7 @@ public class InstanceCreator implements InstanceManager {
      * @param name enum field.
      * @return    algorithm instance.
      */
-    public IAlgorithmStrategy getAlgorithm(Algorithm name) {
+    public IAlgorithmStrategy getAlgorithm(AlgorithmName name) {
 
         return algorithms.get(name);
     }
@@ -58,24 +58,24 @@ public class InstanceCreator implements InstanceManager {
      */
     private void init() {
 
-        algorithms.put(Algorithm.BUCKET_SORT, new BucketSort());
-        algorithms.put(Algorithm.COUNT_WAYS_OF_SUM, new CountWaysOfSum());
-        algorithms.put(Algorithm.COUNTING_SORT, new CountingSort());
-        algorithms.put(Algorithm.FIBONACCI, new Fibonacci());
-        algorithms.put(Algorithm.FRIEND_PAIRS, new FriendPairs());
-        algorithms.put(Algorithm.HIGH_LOW_EFFORTS, new HighLowEfforts());
-        algorithms.put(Algorithm.INSERTION_SORTS, new InsertionSort());
-        algorithms.put(Algorithm.INTERESTING_ROW, new InterestingRow());
-        algorithms.put(Algorithm.LONGEST_SUB_SEQUENCE, new LongestSubSequence());
-        algorithms.put(Algorithm.MERGE_SORT, new MergeSort());
-        algorithms.put(Algorithm.MODIFICATION_FIBONACCI, new ModificationFibonacci());
-        algorithms.put(Algorithm.OPTIMIZED_PAINTING_FENCE, new OptimizedPaintingFence());
-        algorithms.put(Algorithm.PAINTING_THE_FENCE, new PaintingTheFence());
-        algorithms.put(Algorithm.PALINDROME, new Palindrome());
-        algorithms.put(Algorithm.PATHS_WITHOUT_CROSSING, new PathsWithoutCrossing());
-        algorithms.put(Algorithm.QUICK_SORT, new QuickSort());
-        algorithms.put(Algorithm.TILE_THE_FLOOR, new TileTheFloor());
-        algorithms.put(Algorithm.WAYS_TO_COVER_DISTANCE, new WaysToCoverDistance());
-        algorithms.put(Algorithm.WAYS_TO_SUM_TO_N, new WaysToSumToN());
+        algorithms.put(AlgorithmName.BUCKET_SORT, new BucketSort());
+        algorithms.put(AlgorithmName.COUNT_WAYS_OF_SUM, new CountWaysOfSum());
+        algorithms.put(AlgorithmName.COUNTING_SORT, new CountingSort());
+        algorithms.put(AlgorithmName.FIBONACCI, new Fibonacci());
+        algorithms.put(AlgorithmName.FRIEND_PAIRS, new FriendPairs());
+        algorithms.put(AlgorithmName.HIGH_LOW_EFFORTS, new HighLowEfforts());
+        algorithms.put(AlgorithmName.INSERTION_SORTS, new InsertionSort());
+        algorithms.put(AlgorithmName.INTERESTING_ROW, new InterestingRow());
+        algorithms.put(AlgorithmName.LONGEST_SUB_SEQUENCE, new LongestSubSequence());
+        algorithms.put(AlgorithmName.MERGE_SORT, new MergeSort());
+        algorithms.put(AlgorithmName.MODIFICATION_FIBONACCI, new ModificationFibonacci());
+        algorithms.put(AlgorithmName.OPTIMIZED_PAINTING_FENCE, new OptimizedPaintingFence());
+        algorithms.put(AlgorithmName.PAINTING_THE_FENCE, new PaintingTheFence());
+        algorithms.put(AlgorithmName.PALINDROME, new Palindrome());
+        algorithms.put(AlgorithmName.PATHS_WITHOUT_CROSSING, new PathsWithoutCrossing());
+        algorithms.put(AlgorithmName.QUICK_SORT, new QuickSort());
+        algorithms.put(AlgorithmName.TILE_THE_FLOOR, new TileTheFloor());
+        algorithms.put(AlgorithmName.WAYS_TO_COVER_DISTANCE, new WaysToCoverDistance());
+        algorithms.put(AlgorithmName.WAYS_TO_SUM_TO_N, new WaysToSumToN());
     }
 }
